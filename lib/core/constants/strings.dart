e75d7377b7dc8587e650ccf0a149586ce9c5e804
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2023-10-30 21:31:26 +0500

    Progressing

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,8 +1,6 @@
 const packages = 'flutter pub add provider;flutter pub add get;flutter pub add firebase_core;flutter pub add firebase_auth;flutter pub add cloud_firestore;flutter pub add ndialog;flutter pub add firebase_storage;flutter pub add http;flutter pub add shared_preferences;flutter pub add google_sign_in;flutter pub add chat_gpt_sdk';
+const package = '   flutter pub add firebase_core;flutter pub add firebase_auth;flutter pub add cloud_firestore;flutter pub add google_sign_in;flutter pub add ndialog;flutter pub add shared_preferences;flutter pub add firebase_storage;flutter pub add http;flutter pub add sign_in_with_apple;flutter pub add crypto;flutter pub add share_plus;flutter pub add sign_in_with_apple';
 
-/// the above command is to import the packages directly
-///
-///
 
 
 const appTitle = 'My app';
